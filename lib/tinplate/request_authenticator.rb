f98f9127eb80a9bde0cabd621391d7d04b3e06fb
Aaron Klaassen <aaron@crew.co>
2017-02-03 14:37:58 -0600

    Update RequestAuthenticator to handle POST requests.

--- lib/tinplate/request_authenticator.rb
+++ lib/tinplate/request_authenticator.rb
@@ -9,7 +9,7 @@ module Tinplate
     def initialize(action, params = {}, image_name = "")
       @action = action
       @params = params
-      @image_name = image_name
+      @image_name = image_name || ""
       @nonce = SecureRandom.hex
       @date  = Time.now.to_i
     end
@@ -24,16 +24,15 @@ module Tinplate
     end
 
     def verb
-      @image_name ? "POST" : "GET"
+      @image_name.empty? ? "GET" : "POST"
     end
 
     def content_type
-      @verb == "GET" ? "" : "multipart/form-data; boundary=-----------RubyMultipartPost"
-      #@verb == "GET" ? "" : "multipart/form-data; boundary=d8b4f160da95---------------d8b4f160da95"
+      verb == "GET" ? "" : "multipart/form-data; boundary=-----------RubyMultipartPost"
     end
 
     def signature_components
-      s= [
+      [
         Tinplate.configuration.private_key,
         verb,
         content_type,
@@ -43,13 +42,11 @@ module Tinplate
         "https://api.tineye.com/rest/#{@action}/",
         hash_to_sorted_query_string(@params),
       ]
-      #binding.pry
-      s
     end
 
     def signature
-      puts "CORRECT STRING: vibaHBXwUXFqVSg-+kTrqYJZEJkbVeqLc=bo.LlXPOSTmultipart/form-data; boundary=d8b4f160da95---------------d8b4f160da95tineye+logo%281%29.png1350511031wAqXrSG7mJPn5YA6cwDalG.Shttps://api.tineye.com/rest/search/limit=30&offset=0"
-      puts "     MY STRING: #{signature_components.join}"
+      #puts "CORRECT STRING: vibaHBXwUXFqVSg-+kTrqYJZEJkbVeqLc=bo.LlXPOSTmultipart/form-data; boundary=d8b4f160da95---------------d8b4f160da95tineye+logo%281%29.png1350511031wAqXrSG7mJPn5YA6cwDalG.Shttps://api.tineye.com/rest/search/limit=30&offset=0"
+      #puts "     MY STRING: #{signature_components.join}"
 
       OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"),
                               Tinplate.configuration.private_key,
